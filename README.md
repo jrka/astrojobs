507c449ffbee5e57a5caef8bd1880ae81721bc63
Julia Kamenetzky <julia@cactuar.local>
2016-05-12 13:35:24 -0700

    More info in README, slight updates to plot_jobregister.py

--- README.md
+++ README.md
@@ -3,27 +3,30 @@
 Repository for astronomy degree and job related data. Based largely on Metcalfe 2008, PASP, 120, 229-234.
 http://iopscience.iop.org/article/10.1086/528878
 
-TL;DR version: jobregister\_data.csv is a document with all AAS job register data from 
-January 2002 through (currently) May 2016.
+TL;DR version: jobregister\_table.csv is a document with all AAS job register data from 
+January 2002 through (currently) May 2016. Run plot_jobregister.py to see informative plots.
 
 Scripts:
 - search_jobregister.py: requires BeautifulSoup for crawling and scraping.
     If docrawl=True, produces jobregister\_urls.txt.
     If doscrape=True, gather information from each URL in the job register archive.
     Save in groups of 100 as .pkl files.
-    Read in all the .pkl files and create jobregister\_data.csv for further analysis.
+    Read in all the .pkl files and create jobregister\_table.csv for further analysis.
     Not particularly well set up to add incrementally.
     Not necessary to rerun to use analysis; the .txt and .csv files are in the repo.
-- plot\_jobregister.py: produce some summary tables and plots.
+- plot\_jobregister.py: produce some summary tables (see below) and plots (jobregister\_plots.pdf). Requires pandas.
 
-Tables and Sources:
+Tables Included:
 - Produced by search\_jobregister.py:
     - jobregister\_urls.txt 
-    - jobregister\_data.csv
+    - jobregister\_table.csv
 - Update manually:
     - degrees.txt: Estimates of astronomy PhDs produced per year via 3 sources, based on Metcalfe 2008 Table 1.
     - Metcalfe2008_Table2.txt: To add yearly job register data from 1984-2002.
     - funding.txt: Need to add this.
+- Produced by plot\_jobregister.py:
+    - jobregister\_categories_byyear.csv: Number of jobs in each category (see below) by year.
+    - jobregister\_instclass_byyear.csv: Number of jobs in each institution class by year.
 
 Notes on Job Categories: In 2008, the job categorization changed. 
 Here is how I combine the eight pre-2008 categories with the eight post-2008 categories into six new categories.
--- plot_jobregister.py
+++ plot_jobregister.py
@@ -5,11 +5,12 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 # Read in the .csv file as a pandas dataframe 
 df=pd.read_csv('./jobregister_table.csv')
-# Only choose academic year 2003 and 2014, which are complete as of May 2016
+# Only choose academic years between 2003 and 2014 (inclusive), which are complete as of May 2016
 df=df[df.acyear>=2003]
 df=df[df.acyear<=2014]
 
-# Also read in the degree information by year
+# Read in the additional tables.
+# Read in the degree information by year
 deg=pd.read_csv('./degrees.txt',comment='#',delim_whitespace=True,na_values='...')
 deg=deg.rename(columns={'Year':'acyear'})
 deg=deg.set_index('acyear')
@@ -29,7 +30,7 @@ pdf=PdfPages(pdffile)
 # We have 8 categories for institution class, use colorbrewer2.org, 8-class Set1
 color8=['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf']
 
-##### STANDARDIZE TEXT FOR CATEGORIES
+##### STANDARDIZE CATEGORIES, see README.md
 # Check out the available categories each year. Note 2008 is the "crossover" year.
 for yr in set(df.year):
     cats=set(df[df.year==yr]['category'])
@@ -48,12 +49,29 @@ df.category[df.category=='Pre-doctoral/Graduate Positions']='G'
 # Print a few tables
 # http://stackoverflow.com/questions/34907019/pandas-histogram-by-dates-and-sorted-by-categories
 
-###### First table: Job categories by year.
+# First table: Job categories by year.
 table1=df.groupby('acyear')['category'].value_counts().unstack(1)
 # Add a column with "All"
 table1=pd.concat([table1,pd.DataFrame(table1.sum(axis=1),columns=['All'])],axis=1)
 table1=pd.concat([m07t2,table1],axis=0,join='outer')
 table1.to_csv('./jobregister_categories_byyear.csv')
+# Add degree data to table1, jobs per year, for plotting.
+table1=pd.concat([table1,deg],axis=1,join='outer') 
+
+# Second table: Institution Class by Year
+table2=df.groupby('acyear')['instclass'].value_counts().unstack(1).to_csv('./jobregister_instclass_byyear.csv')
+
+
+
+##### START PLOTTING. Totally inconsistent methods here.
+# Plot PhD recipients and funding vs. time. Matches Metcalfe 2008 Figure 1.
+# No funding info yet until we get the inflation indices.
+plt.cla()
+ax=deg.plot(kind='line')
+ax.axvline(2006,color='gray')
+ax.set_xlabel('Academic Year')
+ax.set_ylabel('Astronomy PhDs Awarded')
+pdf.savefig()
 
 # Plot total data here. Matches Metcalfe 2008 Figure 3, jobs per year
 plt.clf()
@@ -63,13 +81,16 @@ plt.plot(table1.index,table1['PV'],label='Postdoc/Research',color='b',linestyle=
 plt.plot(table1.index,table1['NTT'],label='Non-Tenure-Track',color='r',linestyle='--')
 plt.plot(table1.index,table1['RS'],label='Research Support',color='cyan',linestyle='--')
 plt.plot(table1.index,table1['MO'],label='Other',color='magenta',linestyle='-.')
-plt.axvline(2006,color='gray')
+plt.axvline(2006,color='gray') # This indicates where Metcalfe 2008 left off.
+plt.ylabel('Number of Job Postings on AAS Job Register')
+plt.xlabel('Academic Year')
 plt.legend(loc='best')
 pdf.savefig()
 
 # Metcalfe 2008 Figure 3, job register ads / new PhDs
-# Would be best to use UMI measures, but we don't have that.
-table1=pd.concat([table1,deg],axis=1,join='outer')
+# Would be best to use UMI measures, but we don't have that. 
+# Figure out an average ratio between AIP and SED to UMI. Try to use that as an estimate.
+# Do not save this plot.
 plt.clf()
 plt.plot(table1.index,table1['UMI']/table1['AIP'])
 plt.plot(table1.index,table1['UMI']/table1['SED'])
@@ -80,17 +101,19 @@ plt.axhline(avgAIP)
 plt.axhline(avgSED)
 plt.legend(loc='best')
 
+# Now plot job register ads/ new PhDs
 plt.clf()
-plt.plot(table1.index,table1['All']/table1['UMI'],label='All Jobs',color='k')
-plt.plot(table1.index,table1['All']/(table1['SED']*avgSED),label='All Jobs, SED avg')
-plt.plot(table1.index,table1['All']/(table1['AIP']*avgAIP),label='All Jobs, AIP avg')
+plt.plot(table1.index,table1['All']/table1['UMI'],label='All Jobs, using UMI',color='k')
+plt.plot(table1.index,table1['All']/(table1['SED']*avgSED),label='All Jobs, using SED to UMI avg')
+plt.plot(table1.index,table1['All']/(table1['AIP']*avgAIP),label='All Jobs, using AIP to UMI avg')
 plt.legend(loc='best')
+plt.xlabel('Academic Year')
+plt.ylabel('Job Register Ads / New PhD')
 # Come back to this.
 pdf.savefig()
 
 
-###### Second table: Institution Class by Year
-table2=df.groupby('acyear')['instclass'].value_counts().unstack(1).to_csv('./jobregister_instclass_byyear.csv')
+
 
 #### PLOTS AND STATISTICS OF JUST OUR DATA
 # Histogram: Postings by year, broken down by category
